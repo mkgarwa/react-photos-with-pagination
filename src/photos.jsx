9f81d2def373d9a592a7278bc47db9e2830447d7
Manoj Kumar garwa <mkgarwa@gmail.com>
2023-06-08 11:48:49 +0530

    Update photos.jsx
    updated

--- src/photos.jsx
+++ src/photos.jsx
@@ -3,25 +3,30 @@ const Photos = (props) => {
 
   return (
     <table border="1" className="photos-table">
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Thumbnail</th>
-      </tr>
-      {photos.map((data, index) => (
-        <tr key={data.id}>
-          <td>{data.id}</td>
-          <td>{data.title}</td>
-          <td>
-            <img
-              src={data.thumbnailUrl}
-              width="32"
-              height="32"
-              alt="thumbnail"
-            />
-          </td>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Thumbnail</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {photos &&
+          photos.map((data) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.title}</td>
+              <td>
+                <img
+                  src={data.thumbnailUrl}
+                  width="32"
+                  height="32"
+                  alt="thumbnail"
+                />
+              </td>
+            </tr>
+          ))}
+      </tbody>
     </table>
   );
 };
