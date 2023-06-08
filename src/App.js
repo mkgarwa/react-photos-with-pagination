ec5f12b19a2b20d558dda9dc4291febba7520718
Manoj Kumar garwa <mkgarwa@gmail.com>
2023-06-08 11:48:10 +0530

    Update App.js
    integrate custom hook into app.js

--- src/App.js
+++ src/App.js
@@ -3,83 +3,62 @@ import * as constants from "./constants";
 import { useCallback, useEffect, useState } from "react";
 import Photos from "./photos";
 import Pagination from "./pagination";
+import useFetch from "./fetchHook";
 
 export default function App() {
-  const [photos, setAllPhotos] = useState([]);
-  const [currentRecords, setCurrentRecord] = useState([]);
   const [recordPerPage] = useState(50);
   const [currentPage, setCurrentPage] = useState(1);
   const indexOfLastRecord = currentPage * recordPerPage;
   const indexOfFirstRecord = indexOfLastRecord - recordPerPage;
-  const pageCount = Math.ceil(photos.length / recordPerPage);
-
-  const fetchPhotos = () => {
-    fetch(constants.ALL_PHOTOS)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then((response) => {
-        setAllPhotos(response);
-      })
-      .catch((err) => {
-        console.log("No Photos found" + err);
-      });
-  };
-
-  const fetchPerPage = () => {
-    fetch(
-      constants.ALL_PHOTOS +
-        "?_start=" +
-        indexOfFirstRecord +
-        "&_limit=" +
-        recordPerPage
-    )
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then((response) => {
-        setCurrentRecord(response);
-      })
-      .catch((err) => {
-        console.log("No Photos found" + err);
-      });
-  };
+  const [fetchedPhotos] = useFetch(constants.ALL_PHOTOS);
+  let [fetchedCurrentRecords] = useFetch(
+    constants.ALL_PHOTOS +
+      "?_start=" +
+      indexOfFirstRecord +
+      "&_limit=" +
+      recordPerPage
+  );
+  const [photos, setPhotos] = useState([]);
+  const [currentRecords, setCurrentRecords] = useState([]);
+  const pageCount = photos ? Math.ceil(photos.length / recordPerPage) : 0;
 
   useEffect(() => {
-    fetchPhotos();
-  }, []);
+    setPhotos(fetchedPhotos);
+    setCurrentRecords(fetchedCurrentRecords);
+  }, [fetchedPhotos, fetchedCurrentRecords]);
 
-  useEffect(() => {
-    fetchPerPage();
-  }, []);
+  const searchPhotos = useCallback(
+    (searchTerm) => {
+      if (searchTerm === "" || searchTerm.length < 3) {
+        return;
+      }
+      const inputWords = searchTerm.match(/\w+|"[^"]+"/g);
+      inputWords.push(searchTerm);
+      return fetchedPhotos.filter((data) => {
+        const filteredResult = JSON.stringify([
+          data.id,
+          data.title
+        ]).toLowerCase();
+        return inputWords.every((result) => filteredResult.includes(result));
+      });
+    },
+    [fetchedPhotos]
+  );
 
-  const searchPhotos = (searchTerm) => {
-    if (searchTerm === "" || searchTerm.length < 3) {
-      return;
-    }
-    const inputWords = searchTerm.match(/\w+|"[^"]+"/g);
-    inputWords.push(searchTerm);
-    return photos.filter((data) => {
-      const filteredResult = JSON.stringify([
-        data.id,
-        data.title
-      ]).toLowerCase();
-      return inputWords.every((result) => filteredResult.includes(result));
-    });
-  };
-
-  const filterTable = useCallback(({ target }) => {
-    const searchTerm = target.value.toLowerCase();
-    const searchResult = searchPhotos(searchTerm);
-    if (searchResult !== undefined) {
-      setCurrentRecord(searchResult);
-      setAllPhotos(searchResult);
-    }
-  });
+  const filterTable = useCallback(
+    ({ target }) => {
+      const searchTerm = target.value.toLowerCase();
+      const searchResult = searchPhotos(searchTerm);
+      if (searchResult !== undefined) {
+        setCurrentRecords(searchResult);
+        setPhotos(searchResult);
+      } else {
+        setPhotos(fetchedPhotos);
+        setCurrentRecords(fetchedCurrentRecords);
+      }
+    },
+    [searchPhotos, fetchedPhotos, fetchedCurrentRecords]
+  );
 
   return (
     <div className="App">
